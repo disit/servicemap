51d680cd9fb2a99f1ca3d33e3ad3805d7282b9cf
DISIT <info@disit.org>
2020-02-25 11:11:28 +0100

    fixed url

--- install/servicemap-install.sh
+++ install/servicemap-install.sh
@@ -5,6 +5,9 @@ mkdir -p /var/log/tomcat8/servicemap
 chown tomcat8.tomcat8 /var/log/tomcat8/servicemap
 cp servicemap.properties /var/lib/tomcat8/servicemap
 
+#initializes virtuoso
+isql-vt localhost dba dba ServiceMap.vt
+
 #ingest museums and historical building from dbpedia
 isql-vt localhost dba dba servicemap-dbpedia.vt
 
